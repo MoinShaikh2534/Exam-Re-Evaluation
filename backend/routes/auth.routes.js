48b99c777e108f2e13759ac6b3bbdc27bfabe106
Shreyash Galgale <shreyasgalgale019@gmail.com>
2025-02-19 20:21:55 +0530

    merge

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,11 +3,6 @@ const {
     studentLogin,
     studentRegister,
 } = require("../controllers/auth.controllers");
-const router = express.Router();
-
-router.post("/student-register", studentRegister);
-//student login
-router.post("/student-login", studentLogin);
 const { authMiddleware } = require("../middlewares/authMiddlewares");
 const {
     registerFaculty,
@@ -16,10 +11,15 @@ const {
     getAllFaculty,
     logoutFaculty,
 } = require("../controllers/authcontrollers");
+const router = express.Router();
+
+router.post("/student-register", studentRegister);
+router.post("/student-login", studentLogin);
 
 router.post("/faculty/register", registerFaculty);
 router.post("/faculty/login", loginFaculty);
 router.post("/faculty/logout", logoutFaculty);
 router.get("/faculty/profile", authMiddleware, getFacultyProfile);
 router.get("/faculty/all", authMiddleware, getAllFaculty);
+
 module.exports = router;
